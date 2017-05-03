ab54b49c1cc163bcfaa7ed015fabd501c0b572e1
JonasBusseniers <j_busseniers@hotmail.com>
2017-05-03 15:55:33 +0200

    first sql statement

--- src/be/vdab/repositories/GastenboekRepository.java
+++ src/be/vdab/repositories/GastenboekRepository.java
@@ -1,5 +1,5 @@
 package be.vdab.repositories;
 
 public class GastenboekRepository extends AbstractRepository {
-
+	private static final String READ_ALL = "select date, name, message from  gastenboek";
 }
